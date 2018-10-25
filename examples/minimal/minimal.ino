6acdb1c8f4492d90bbe130b99964d709ea0b84d5
GaryLim <TseSiangGary.Lim@infineon.com>
2018-10-25 16:10:45 +0800

    Update examples/minimal/minimal.ino
    Added the bare minimal example

---
+++ examples/minimal/minimal.ino
@@ -0,0 +1,48 @@
+/**
+ * MIT License
+ *
+ * Copyright (c) 2018 Infineon Technologies AG
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ *  copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE
+ *
+ * Demonstrates use of the 
+ * Infineon Technologies AG OPTIGA™ Trust X Arduino library
+ */
+void setup() 
+{ 
+  /*
+   * Initialise a serial port for debug output
+   */
+  Serial.begin(115200, SERIAL_8N1);
+  delay(1000);
+  Serial.println("Initializing XMC... ");
+
+  //Set LED GPIO as output
+  pinMode(14, OUTPUT);
+  pinMode(15, OUTPUT);
+  
+}
+
+void loop()
+{     
+   digitalWrite(14, HIGH); // turn the LED1 on (HIGH is the voltage level) 
+   digitalWrite(15, HIGH); // turn the LED2 on (HIGH is the voltage level) 
+
+   Serial.println("Minimal setup completed. Looks like you are ready to start!");
+}
