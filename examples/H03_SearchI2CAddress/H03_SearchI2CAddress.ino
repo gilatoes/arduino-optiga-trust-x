3d770fb84d74d724729dc0bd799728298bc438d9
GaryLim <TseSiangGary.Lim@infineon.com>
2019-01-27 21:49:07 +0800

    I2C Search Address
    Add helper function to search Trust X I2C address

---
+++ examples/H03_SearchI2CAddress/H03_SearchI2CAddress.ino
@@ -0,0 +1,97 @@
+/* This source code is forked from Infineon GitHub, frozen and modified for training purposes. 
+ * Therefore, it is neither updated regularly nor qualify for production. 
+ * Please refer to official Infineon GitHub https://github.com/Infineon/arduino-optiga-trust-x 
+ * for the updated and latest release. 
+ */
+/**
+ * MIT License
+ *
+ * Copyright (c) 2019 Infineon Technologies AG
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ *  copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE
+ *
+ * Demonstrates use of the
+ * Infineon Technologies AG OPTIGA™ Trust X Arduino library
+ */
+
+#include "OPTIGATrustX.h"
+#include <Arduino.h>
+#include "debug.h"
+
+uint8_t sys_init =0;
+void setup()
+{
+  Serial.begin(115200, SERIAL_8N1);
+  delay(100);
+
+ /*
+   * Initialise an OPTIGA™ Trust X Board
+   */
+  if(reset()==0){
+    sys_init=1;
+  }else{
+    sys_init=0;
+  }
+}
+
+uint8_t reset()
+{
+  return 0;
+}
+
+
+void loop()
+{
+  uint32_t ret = 0;
+
+  if(sys_init)
+  {
+    Serial.println("Searching Trust X Address:");
+  
+    for(int i=0; i< 0x7F; i++){
+      ret = trustX.begin(i);
+      if (ret) {
+        //Serial.println("Failed");
+      }else{
+      Serial.print("\r\nAddress found: 0x");
+      Serial.println(i, HEX);
+      }
+    }
+    //close the connection
+    trustX.end();
+  }
+  Serial.println("\r\nPress i to re-initialize..");
+  while (Serial.available()==0){} //Wait for user input
+  String input = Serial.readString();  //Reading the Input string from Serial port.
+  input.trim();
+  if(input=="i")
+  {
+    if(reset()!=0)
+    {
+      //Do not execute
+      sys_init=0;
+      //close the connection
+      trustX.end();
+    }else
+    {
+      sys_init=1;
+      }
+  }
+
+}
