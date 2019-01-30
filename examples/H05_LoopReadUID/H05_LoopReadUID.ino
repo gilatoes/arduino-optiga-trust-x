2dd04fc2f8a21aeb32e36d2b5c08c150c65cde17
GaryLim <TseSiangGary.Lim@infineon.com>
2019-01-30 10:22:17 +0800

    Implements loop operation
    Perform loop operation on Trust X

---
+++ examples/H05_LoopReadUID/H05_LoopReadUID.ino
@@ -0,0 +1,124 @@
+/* This source code is forked from Infineon GitHub, frozen and modified for training purposes. 
+ * Therefore, it is neither updated regularly nor qualify for production. 
+ * Please refer to official Infineon GitHub https://github.com/Infineon/arduino-optiga-trust-x 
+ * for the updated and latest release. 
+ */
+/**
+ * MIT License
+ *
+ * Copyright (c) 2019 Infineon Technologies AG
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ *  copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE
+ *
+ * Demonstrates use of the
+ * Infineon Technologies AG OPTIGA™ Trust X Arduino library
+ */
+
+#include "OPTIGATrustX.h"
+#include <Arduino.h>
+#include "debug.h"
+
+uint8_t sys_init =0;
+uint16_t UID_LENGTH=27;
+void setup()
+{
+  /*
+   * Initialise a serial port for debug output
+   */
+  Serial.begin(115200, SERIAL_8N1);
+  delay(100);
+ /*
+   * Initialise an OPTIGA™ Trust X Board
+   */
+  if(reset()==0){
+    sys_init=1;
+  }else{
+    sys_init=0;
+  }
+}
+
+uint8_t reset()
+{
+/*  
+  uint32_t ret = 0;
+  //Serial.println("Initialize Trust X");
+  //ret = trustX.begin();
+  if (ret) {
+    Serial.print("Failed");
+    return -1;
+  }  
+*/
+  return 0;
+}
+
+void loop()
+{
+   uint32_t ret=0;
+   uint8_t  uid[UID_LENGTH];
+   uint16_t passed=0;
+   uint16_t test_count=100;
+
+  if(sys_init)
+  {
+
+    Serial.println("Initialize Trust X");
+
+    for(int i=0;i<test_count; i++){
+            
+    ret = trustX.begin();    
+    Serial.println("\r\nGetting co-processor Unique ID:");
+    ret = trustX.getUniqueID(uid, UID_LENGTH);
+
+    if (ret) {
+      Serial.println("Failed");
+      Serial.println(ret,HEX);
+      //close the connection
+      trustX.end();
+    }else{
+    HEXDUMP(uid, UID_LENGTH);
+    passed++;
+    }
+
+    Serial.print("current loop count: ");
+    Serial.println((i+1), DEC);
+      
+    trustX.end(); 
+    }
+
+    Serial.print("Test Completed (passed/total): ");
+    Serial.print(passed, DEC);
+    Serial.print("/");
+    Serial.println(test_count, DEC);
+
+  }
+  Serial.println("\r\nPress i to re-initialize.. other key to loop...");
+  while (Serial.available()==0){} //Wait for user input
+  String input = Serial.readString();  //Reading the Input string from Serial port.
+  input.trim();
+  if(input=="i"){
+    if(reset()!=0){
+      //Do not execute
+      sys_init=0;
+      //close the connection
+      trustX.end();
+    }else{
+      sys_init=1;
+      }
+  }
+}
---
+++ examples/H05_LoopReadUID/debug.h
@@ -0,0 +1,167 @@
+/* This source code is forked from Infineon GitHub, frozen and modified for training purposes. 
+ * Therefore, it is neither updated regularly nor qualify for production. 
+ * Please refer to official Infineon GitHub https://github.com/Infineon/arduino-optiga-trust-x 
+ * for the updated and latest release. 
+ */
+/**
+ * MIT License
+ *
+ * Copyright (c) 2019 Infineon Technologies AG
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ *  copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE
+ *
+ * Demonstrates use of the
+ * Infineon Technologies AG OPTIGA™ Trust X Arduino library
+ */
+#ifndef FPRINT_H
+#define FPRINT_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#include "Arduino.h"
+
+#define MAXCMD_LEN      255
+#define HEXDUMP_COLS      16
+
+#ifndef SUPPRESSHEXDUMP
+#define SUPPRESSHEXDUMP   0
+#else
+#define SUPPRESSHEXDUMP   1
+#endif
+#define HEXDUMP(a, b)   (SUPPRESSHEXDUMP==0) ? __hexdump__(a,b) : (void) 0;
+
+#define HEXONLYDUMP(a, b)   __hexonlydump__(a,b)
+
+/**
+ *
+ * Printout data in a standard hex view
+ *
+ * @param[in] p_buf   Pointer to data which should be printed out.
+ * @param[in] l_len   Length of a data
+ *
+ * @retval  None
+ * @example  
+ 0x000000: 2e 2f 68 65 78 64 75 6d ./hexdum
+ 0x000008: 70 00 53 53 48 5f 41 47 p.SSH_AG
+ 0x000010: 45 4e 54 5f             ENT_
+ */
+inline void __hexdump__(const void* p_buf, uint32_t l_len) {
+  unsigned int i, j;
+  static char str[MAXCMD_LEN];
+  for (i = 0; i < l_len + ((l_len % HEXDUMP_COLS) ?
+          ( HEXDUMP_COLS - l_len % HEXDUMP_COLS) : 0);
+      i++) {
+    /* print offset */
+    if (i % HEXDUMP_COLS == 0) {
+      sprintf(str, "0x%06x: ", i);
+      Serial.print(str);
+    }
+
+    /* print hex data */
+    if (i < l_len) {
+      sprintf(str, "%02x ", 0xFF & ((char*) p_buf)[i]);
+      Serial.print(str);
+    } else /* end of block, just aligning for ASCII dump */
+    {
+      sprintf(str, "   ");
+      Serial.print(str);
+    }
+
+    /* print ASCII dump */
+    if (i % HEXDUMP_COLS == ( HEXDUMP_COLS - 1)) {
+      for (j = i - ( HEXDUMP_COLS - 1); j <= i; j++) {
+        if (j >= l_len) /* end of block, not really printing */
+        {
+          Serial.print(' ');
+        } else if (isprint((int) ((char*) p_buf)[j])) /* printable char */
+        {
+          Serial.print(((char*) p_buf)[j]);
+        } else /* other char */
+        {
+          Serial.print('.');
+        }
+      }
+      Serial.print('\r');
+      Serial.print('\n');
+      delay(10);
+    }
+  }
+}
+
+/**
+ *
+ * Printout data for easy cut and paste
+ *
+ * @param[in] p_buf   Pointer to data which should be printed out.
+ * @param[in] l_len   Length of a data
+ *
+ * @retval  None
+ * @example  
+2e 2f 68 65 78 64 75 6d 
+70 00 53 53 48 5f 41 47 
+45 4e 54 5f             
+ */
+inline void __hexonlydump__(const void* p_buf, uint32_t l_len) {
+  unsigned int i, j;
+  static char str[MAXCMD_LEN];
+  for (i = 0; i < l_len + ((l_len % HEXDUMP_COLS) ?
+          ( HEXDUMP_COLS - l_len % HEXDUMP_COLS) : 0);
+      i++) {
+    /* print offset */
+    if (i % HEXDUMP_COLS == 0) {
+      //sprintf(str, "0x%06x: ", i);
+      //Serial.print(str);
+    }
+
+    /* print hex data */
+    if (i < l_len) {
+      sprintf(str, "%02x ", 0xFF & ((char*) p_buf)[i]);
+      Serial.print(str);
+    } else /* end of block, just aligning for ASCII dump */
+    {
+      sprintf(str, "   ");
+      Serial.print(str);
+    }
+
+    /* print ASCII dump */
+    if (i % HEXDUMP_COLS == ( HEXDUMP_COLS - 1)) {
+      for (j = i - ( HEXDUMP_COLS - 1); j <= i; j++) {
+        if (j >= l_len) /* end of block, not really printing */
+        {
+          Serial.print(' ');
+        } else if (isprint((int) ((char*) p_buf)[j])) /* printable char */
+        {
+          //Serial.print(((char*) p_buf)[j]);
+        } else /* other char */
+        {
+          //Serial.print('.');
+        }
+      }
+      Serial.print('\r');
+      Serial.print('\n');
+    }
+    delay(10);
+  }
+}
+#ifdef __cplusplus
+}
+#endif
+#endif
